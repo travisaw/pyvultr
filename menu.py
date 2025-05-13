f3d7999d24ef631288c27273880f0a92fdc7bc2b
Travis <travis@travisty.org>
2025-05-13 15:19:58 -0400

    Work on instances endpoint

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 
-from account import get_account_info
+from endpoints.account import get_account_info
+from endpoints.instances import get_instances
 
 class Menu():
     def __init__(self, api):
@@ -7,16 +8,19 @@ class Menu():
 
     def main_menu(self):
         print('1. Account')
-        print('2. Firewall')
-        print('3. Exit')
+        print('2. Instances')
+        print('3. Firewall')
+        print('4. Exit')
         option = input("What area?: ")
         match option:
             case '1':
                 self.account()
             case '2':
+                self.instances()
+            case '3':
                 print('Coming Soon!')
                 self.main_menu()
-            case '3':
+            case '4':
                 exit()
             case _:
                 print('Invalid Option!')
@@ -35,3 +39,17 @@ class Menu():
             case _:
                 print('Invalid Option!')
                 self.account()
+
+    def instances(self):
+        print('1. Show Instances')
+        print('2. Go Back')
+        option = input("What area?: ")
+        match option:
+            case '1':
+                get_instances(self.api)
+                self.instances()
+            case '2':
+                self.main_menu()
+            case _:
+                print('Invalid Option!')
+                self.instances()
